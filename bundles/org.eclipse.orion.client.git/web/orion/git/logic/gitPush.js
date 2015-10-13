10517990f14a95ac36f50cef98e72766049f3140
Victor Chu <vchu@palantir.com>
2015-10-13 15:24:33 -0700

    ORION-79 Show logs from post-receive hook

--- bundles/org.eclipse.orion.client.git/web/orion/git/logic/gitPush.js
+++ bundles/org.eclipse.orion.client.git/web/orion/git/logic/gitPush.js
@@ -174,6 +174,9 @@ define([
 													}
 												}
 											}
+											if (jsonData.Message) {
+												display.Message +=  ".</br></br>Remote responded with: " + jsonData.Message;
+											}
 										}
 										serviceRegistry.getService("orion.page.message").setProgressResult(display); //$NON-NLS-0$
 									}
